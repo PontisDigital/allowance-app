7db01a91ab20e9ed13f27349a482c1278b7f086e
Sam Collier <sam@samcollier.tech>
2023-08-01 23:51:08 +0100

    rename widgets + navigate from landing to onboarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
-import 'dart:ui';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/page-1/onboarding-1-done.dart';
+
 import 'package:myapp/utils.dart';
 import 'package:myapp/page-1/loading-page-done.dart';
-// import 'package:myapp/page-1/search-page-done.dart';
-// import 'package:myapp/page-1/home-page-done.dart';
-// import 'package:myapp/page-1/settings-1-done.dart';
-// import 'package:myapp/page-1/settings-2-done.dart';
-// import 'package:myapp/page-1/settings-3-done.dart';
-// import 'package:myapp/page-1/settings-4-done.dart';
-//import 'package:myapp/page-1/onboarding-1-done.dart';
-// import 'package:myapp/page-1/onboarding-2-done.dart';
-// import 'package:myapp/page-1/settings-5-done.dart';
-// import 'package:myapp/page-1/pay-1-done.dart';
-// import 'package:myapp/page-1/send-2-done.dart';
-// import 'package:myapp/page-1/send-1-done.dart';
+import 'package:myapp/page-1/home-page-done.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
 	@override
 	Widget build(BuildContext context) {
-	return MaterialApp(
-		title: 'Flutter',
-		debugShowCheckedModeBanner: false,
-		scrollBehavior: MyCustomScrollBehavior(),
-		theme: ThemeData(
-		primarySwatch: Colors.blue,
-		),
-		home: Scaffold(
-		body: SingleChildScrollView(
-			child: Scene(),
-		),
-		),
-	);
+		bool isLoggedIn = true;
+		return MaterialApp(
+			title: 'Flutter',
+			debugShowCheckedModeBanner: false,
+			scrollBehavior: MyCustomScrollBehavior(),
+			theme: ThemeData(
+			primarySwatch: Colors.blue,
+			),
+			home: Scaffold(
+				body: SingleChildScrollView(
+					child: isLoggedIn ? HomePage() : LandingPage(),
+				),
+			),
+		);
 	}
 }
--- lib/page-1/loading-page-done.dart
+++ lib/page-1/loading-page-done.dart
@@ -2,74 +2,86 @@ import 'package:flutter/material.dart';
 import 'package:flutter/gestures.dart';
 import 'dart:ui';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:myapp/page-1/onboarding-1-done.dart';
 import 'package:myapp/utils.dart';
 
-class Scene extends StatelessWidget {
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
+
+  void navigateToOnboarding(BuildContext context)
+  {
+	Navigator.push(context,
+	  MaterialPageRoute(builder: (context) => OnboardingStartPage()));
+  }
+
   @override
   Widget build(BuildContext context) {
-    double baseWidth = 390;
-    double fem = MediaQuery.of(context).size.width / baseWidth;
-    double ffem = fem * 0.97;
-    return Container(
-      width: double.infinity,
-      child: Container(
-        // loadingpagedoneeTh (301:827)
-        padding: EdgeInsets.fromLTRB(71*fem, 339*fem, 71*fem, 339*fem),
-        width: double.infinity,
-        height: 844*fem,
-        decoration: BoxDecoration (
-          color: Color(0xff041e42),
-        ),
-        child: Container(
-          // frame37160U5M (301:828)
-          width: double.infinity,
-          height: double.infinity,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                // allowanceNwR (301:829)
-                margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 4*fem),
-                child: Text(
-                  'allowance.',
-                  textAlign: TextAlign.center,
-                  style: SafeGoogleFont (
-                    'Outfit',
-                    fontSize: 50*ffem,
-                    fontWeight: FontWeight.w700,
-                    height: 1.26*ffem/fem,
-                    color: Color(0xffffffff),
-                  ),
-                ),
-              ),
-              Container(
-                // tapanywhereECw (301:830)
-                margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 9*fem),
-                child: Text(
-                  'tap anywhere',
-                  textAlign: TextAlign.center,
-                  style: SafeGoogleFont (
-                    'PT Sans Caption',
-                    fontSize: 15*ffem,
-                    fontWeight: FontWeight.w400,
-                    height: 1.2925*ffem/fem,
-                    color: Color(0x87ffffff),
-                  ),
-                ),
-              ),
-              Container(
-                // jackthebulldogw7M (301:831)
-                width: 70*fem,
-                height: 70*fem,
-                child: Image.asset(
-                  'assets/page-1/images/jack-the-bulldog.png',
-                  fit: BoxFit.cover,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-          );
+	double baseWidth = 390;
+	double fem = MediaQuery.of(context).size.width / baseWidth;
+	double ffem = fem * 0.97;
+	return GestureDetector(
+	  onTap: () => navigateToOnboarding(context),
+	  child: SizedBox(
+		width: double.infinity,
+		child: Container(
+		  // loadingpagedoneeTh (301:827)
+		  padding: EdgeInsets.fromLTRB(71*fem, 339*fem, 71*fem, 339*fem),
+		  width: double.infinity,
+		  height: 844*fem,
+		  decoration: const BoxDecoration (
+			color: Color(0xff041e42),
+		  ),
+		  child: SizedBox(
+			// frame37160U5M (301:828)
+			width: double.infinity,
+			height: double.infinity,
+			child: Column(
+			  crossAxisAlignment: CrossAxisAlignment.center,
+			  children: [
+				Container(
+				  // allowanceNwR (301:829)
+				  margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 4*fem),
+				  child: Text(
+					'allowance.',
+					textAlign: TextAlign.center,
+					style: SafeGoogleFont (
+					  'Outfit',
+					  fontSize: 50*ffem,
+					  fontWeight: FontWeight.w700,
+					  height: 1.26*ffem/fem,
+					  color: const Color(0xffffffff),
+					),
+				  ),
+				),
+				Container(
+				  // tapanywhereECw (301:830)
+				  margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 9*fem),
+				  child: Text(
+					'tap anywhere',
+					textAlign: TextAlign.center,
+					style: SafeGoogleFont (
+					  'PT Sans Caption',
+					  fontSize: 15*ffem,
+					  fontWeight: FontWeight.w400,
+					  height: 1.2925*ffem/fem,
+					  color: const Color(0x87ffffff),
+					),
+				  ),
+				),
+				SizedBox(
+				  // jackthebulldogw7M (301:831)
+				  width: 70*fem,
+				  height: 70*fem,
+				  child: Image.asset(
+					'assets/page-1/images/jack-the-bulldog.png',
+					fit: BoxFit.cover,
+				  ),
+				),
+			  ],
+			),
+		  ),
+		),
+	  ),
+	);
   }
 }
