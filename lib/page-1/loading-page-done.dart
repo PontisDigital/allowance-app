ecc373d4f355ac12813e4d4aca29c2449cc533e8
Sam Collier <sam@samcollier.tech>
2023-08-17 00:46:22 +0100

    tablet optimizations

--- lib/page-1/loading-page-done.dart
+++ lib/page-1/loading-page-done.dart
@@ -1,87 +1,71 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
-import 'dart:ui';
-import 'package:google_fonts/google_fonts.dart';
-import 'package:allowance/utils.dart';
 import 'package:allowance/page-1/onboarding-start.dart';
 
 class LandingPage extends StatelessWidget {
-  const LandingPage({super.key});
+  const LandingPage({Key? key}) : super(key: key);
 
-  void navigateToOnboarding(BuildContext context)
-  {
-	Navigator.pushReplacement(context,
-	  MaterialPageRoute(builder: (context) => OnboardingStartPage()));
+  void navigateToOnboarding(BuildContext context) {
+    Navigator.pushReplacement(
+      context,
+      MaterialPageRoute(builder: (context) => OnboardingStartPage()),
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-	double baseWidth = 390;
-	double fem = MediaQuery.of(context).size.width / baseWidth;
-	double ffem = fem * 0.97;
-	return GestureDetector(
-	  onTap: () => navigateToOnboarding(context),
-	  child: SizedBox(
-		width: double.infinity,
-		child: Container(
-		  // loadingpagedoneeTh (301:827)
-		  padding: EdgeInsets.fromLTRB(71*fem, 339*fem, 71*fem, 339*fem),
-		  width: double.infinity,
-		  height: 844*fem,
-		  decoration: const BoxDecoration (
-			color: Color(0xff041e42),
-		  ),
-		  child: SizedBox(
-			// frame37160U5M (301:828)
-			width: double.infinity,
-			height: double.infinity,
-			child: Column(
-			  crossAxisAlignment: CrossAxisAlignment.center,
-			  children: [
-				Container(
-				  // allowanceNwR (301:829)
-				  margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 4*fem),
-				  child: Text(
-					'allowance.',
-					textAlign: TextAlign.center,
-					style: SafeGoogleFont (
-					  'Outfit',
-					  fontSize: 50*ffem,
-					  fontWeight: FontWeight.w700,
-					  height: 1.26*ffem/fem,
-					  color: const Color(0xffffffff),
-					),
-				  ),
-				),
-				Container(
-				  // tapanywhereECw (301:830)
-				  margin: EdgeInsets.fromLTRB(0*fem, 0*fem, 0*fem, 9*fem),
-				  child: Text(
-					'tap anywhere',
-					textAlign: TextAlign.center,
-					style: SafeGoogleFont (
-					  'PT Sans Caption',
-					  fontSize: 15*ffem,
-					  fontWeight: FontWeight.w400,
-					  height: 1.2925*ffem/fem,
-					  color: const Color(0x87ffffff),
-					),
-				  ),
-				),
-				SizedBox(
-				  // jackthebulldogw7M (301:831)
-				  width: 70*fem,
-				  height: 70*fem,
-				  child: Image.asset(
-					'assets/page-1/images/jack-the-bulldog.png',
-					fit: BoxFit.cover,
-				  ),
-				),
-			  ],
-			),
-		  ),
-		),
+    final double baseWidth = 390;
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double fem = screenWidth / baseWidth;
+    final double ffem = fem * 0.97;
+
+    return SingleChildScrollView(child: GestureDetector(
+      onTap: () => navigateToOnboarding(context),
+      child: Container(
+        padding: EdgeInsets.symmetric(
+          horizontal: 71 * fem,
+          vertical: 339 * fem,
+        ),
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Color(0xff041e42),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              'allowance.',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 50 * ffem,
+                fontWeight: FontWeight.w700,
+                height: 1.26,
+                color: Colors.white,
+              ),
+            ),
+            SizedBox(height: 9 * fem),
+            Text(
+              'tap anywhere',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 15 * ffem,
+                fontWeight: FontWeight.w400,
+                height: 1.2925,
+                color: Colors.white.withOpacity(0.7),
+              ),
+            ),
+            SizedBox(height: 20 * fem), // Adjust spacing as needed
+            Image.asset(
+              'assets/page-1/images/jack-the-bulldog.png',
+              width: 70 * fem,
+              height: 70 * fem,
+              fit: BoxFit.cover,
+            ),
+          ],
+        ),
+      ),
 	  ),
-	);
+    );
   }
 }
+
--- lib/page-1/send-1-done.dart
+++ lib/page-1/send-1-done.dart
@@ -117,91 +117,93 @@ class _SendSelectUserPageState extends State<SendSelectUserPage> {
 		  ),
 		),
       ),
-      body: Container(
-        width: double.infinity,
+      body: SingleChildScrollView(
         child: Container(
-          padding: EdgeInsets.fromLTRB(33 * fem, 34 * fem, 33 * fem, 95 * fem),
           width: double.infinity,
-          decoration: BoxDecoration(
-            color: Color(0xff041e42),
-          ),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                margin:
-                    EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 13 * fem),
-                child: Text(
-                  'search for your friends',
-                  textAlign: TextAlign.center,
-                  style: SafeGoogleFont(
-                    'Outfit',
-                    fontSize: 25 * ffem,
-                    fontWeight: FontWeight.w700,
-                    height: 1.26 * ffem / fem,
-                    color: Color(0xffffffff),
+          child: Container(
+            padding: EdgeInsets.fromLTRB(33 * fem, 34 * fem, 33 * fem, 95 * fem),
+            width: double.infinity,
+            decoration: BoxDecoration(
+              color: Color(0xff041e42),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  margin:
+                      EdgeInsets.fromLTRB(0 * fem, 0 * fem, 0 * fem, 13 * fem),
+                  child: Text(
+                    'search for your friends',
+                    textAlign: TextAlign.center,
+                    style: SafeGoogleFont(
+                      'Outfit',
+                      fontSize: 25 * ffem,
+                      fontWeight: FontWeight.w700,
+                      height: 1.26 * ffem / fem,
+                      color: Color(0xffffffff),
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Container(
-                      margin: EdgeInsets.fromLTRB(
-                          0 * fem, 0 * fem, 0 * fem, 9 * fem),
-                      width: double.infinity,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(10 * fem),
-                        color: Color(0xcc446da3),
-                      ),
-                      child: Card(
-                        color: Colors.transparent,
-                        child: TextField(
-                          controller: _textFieldController,
-                          style: TextStyle(color: Colors.white),
-                          onChanged: (value) => {_updateInputValue(value)},
-                          decoration: InputDecoration(
-                            border: InputBorder.none,
-                            focusedBorder: InputBorder.none,
-                            enabledBorder: InputBorder.none,
-                            errorBorder: InputBorder.none,
-                            disabledBorder: InputBorder.none,
-                            contentPadding: EdgeInsets.fromLTRB(
-                                7 * fem, 1 * fem, 7 * fem, 0 * fem),
+                Container(
+                  width: double.infinity,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(
+                        margin: EdgeInsets.fromLTRB(
+                            0 * fem, 0 * fem, 0 * fem, 9 * fem),
+                        width: double.infinity,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10 * fem),
+                          color: Color(0xcc446da3),
+                        ),
+                        child: Card(
+                          color: Colors.transparent,
+                          child: TextField(
+                            controller: _textFieldController,
+                            style: TextStyle(color: Colors.white),
+                            onChanged: (value) => {_updateInputValue(value)},
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              focusedBorder: InputBorder.none,
+                              enabledBorder: InputBorder.none,
+                              errorBorder: InputBorder.none,
+                              disabledBorder: InputBorder.none,
+                              contentPadding: EdgeInsets.fromLTRB(
+                                  7 * fem, 1 * fem, 7 * fem, 0 * fem),
+                            ),
                           ),
                         ),
                       ),
-                    ),
-                    Container(
-                      padding: EdgeInsets.fromLTRB(
-                          10 * fem, 8 * fem, 9.08 * fem, 8 * fem),
-                      width: double.infinity,
-                      height: 636 * fem,
-                      decoration: BoxDecoration(
-                        image: DecorationImage(
-                          fit: BoxFit.cover,
-                          image: AssetImage(
-                            'assets/page-1/images/frame-37141.png',
+                      Container(
+                        padding: EdgeInsets.fromLTRB(
+                            10 * fem, 8 * fem, 9.08 * fem, 8 * fem),
+                        width: double.infinity,
+                        height: 636 * fem,
+                        decoration: BoxDecoration(
+                          image: DecorationImage(
+                            fit: BoxFit.cover,
+                            image: AssetImage(
+                              'assets/page-1/images/frame-37141.png',
+                            ),
                           ),
                         ),
-                      ),
-                      child: Column(
-                        children: _userList
-                            .map((user) => UserCard(
-                                  username: user.username,
-                                  imageUrl: null,
-                                  isButton: true,
+                        child: Column(
+                          children: _userList
+                              .map((user) => UserCard(
+                                    username: user.username,
+                                    imageUrl: null,
+                                    isButton: true,
 								  currentBalance: widget.currentBalance,
-                                ))
-                            .toList(),
+                                  ))
+                              .toList(),
+                        ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
