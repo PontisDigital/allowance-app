8e5f221950652fdb99a576be8c3ecfdec81b888f
Sam Collier <sam@samcollier.tech>
2023-08-20 01:18:58 +0100

    add coming soon cards

--- lib/coming_soon.dart
+++ lib/coming_soon.dart
@@ -1,9 +1,14 @@
+import 'package:allowance/home_allowance_entry.dart';
 import 'package:allowance/page-1/home-page-done.dart';
-import 'package:allowance/page-1/search-page-done.dart';
 import 'package:allowance/utils.dart';
+import 'package:blur/blur.dart';
 import 'package:flutter/material.dart';
 
 class ComingSoonWidget extends StatelessWidget {
+  final String imageUrl;
+
+  ComingSoonWidget({required this.imageUrl});
+
   @override
   Widget build(BuildContext context) {
     const double baseWidth = 386.4799804688;
@@ -12,23 +17,27 @@ class ComingSoonWidget extends StatelessWidget {
     final imageWidth = 104.5;
     final imageHeight = 74.8;
 
-    return Container(
-      // storecardmQs (27:44)
-      margin: EdgeInsets.fromLTRB(0.79 * fem, 0 * fem, 0.79 * fem, 0 * fem),
-      padding: EdgeInsets.fromLTRB(11 * fem, 15 * fem, 11 * fem, 36 * fem),
-      width: double.infinity,
-	  height: 150,
-      decoration: BoxDecoration(
+    return Stack(children: [
+      Blur(
+        blur: 8,
+        blurColor: Colors.black,
         borderRadius: BorderRadius.circular(26.3999996185 * fem),
-        color: Colors.grey.shade600,
+        child: StoreCardWidget(
+            allowance: Allowance(
+                balance: "\$0.00",
+                imageUrl: imageUrl,
+                    
+					)),
       ),
-      child: Center(
-          child: Text("More Stores Coming Soon!", style: SafeGoogleFont(
-		  'Outfit',
-		  fontSize: 20,
-		  fontWeight: FontWeight.w700,
-		  color: Colors.white,
-		  ))),
-    );
+      Positioned.fill(
+          child: Center(
+              child: Text("Coming Soon!",
+                  style: SafeGoogleFont(
+                    'Outfit',
+                    fontSize: 20,
+                    fontWeight: FontWeight.w700,
+                    color: Colors.white,
+                  )))),
+    ]);
   }
 }
