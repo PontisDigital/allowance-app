a979cb84f77c406602cb44959dbc1b5d03ce1af8
Sam Collier <sam@samcollier.tech>
2023-08-22 03:53:48 +0100

    make survey required

--- lib/page-1/home-page-done.dart
+++ lib/page-1/home-page-done.dart
@@ -6,7 +6,7 @@ import 'package:allowance/home_allowance_entry.dart';
 import 'package:allowance/more_info.dart';
 import 'package:allowance/page-1/loading-page-done.dart';
 import 'package:allowance/page-1/pay-1-done.dart';
-import 'package:allowance/survey_prompt.dart';
+import 'package:allowance/survey.dart';
 import 'package:flutter/material.dart';
 import 'package:allowance/utils.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -282,8 +282,11 @@ class _HomePageState extends State<HomePage> {
 
         if (!tookSurvey && !widget.surveyIsOpen) {
           widget.surveyIsOpen = true;
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => SurveyPrompt()));
+          Navigator.pushAndRemoveUntil(
+            context,
+            MaterialPageRoute(builder: (context) => HopSurvey()),
+            (route) => false,
+          );
         }
       } else {
         // Handle API error here
