91883ed9e45fbfbb7bc3156d5867c9b165e1f2b8
Sam Collier <sam@samcollier.tech>
2023-08-28 21:14:43 +0100

    prevent wrapping on community page

--- lib/community_user_card.dart
+++ lib/community_user_card.dart
@@ -19,6 +19,9 @@ class UserCardCommunity extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    double baseWidth = 386.4799804688;
+    double fem = MediaQuery.of(context).size.width / baseWidth;
+    double ffem = fem * 0.97;
     return Card(
       elevation: 3,
       //color: Colors.grey.shade700,
@@ -42,7 +45,7 @@ class UserCardCommunity extends StatelessWidget {
           (username == 'Someone') ? 'Someone' : '@${username}',
           style: SafeGoogleFont(
             'Outfit',
-            fontSize: 20,
+			fontSize: 18 * ffem,
             color: Colors.white,
           ),
         ),
